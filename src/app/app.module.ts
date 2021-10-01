a1ca4a1067058b44dcfd435026dbb9f27e0521cf
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-01 19:45:19 -0400

    Completion of step 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+// basic imports Angular starts with
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+
+// Imports through tutorial directions for
+import { FormsModule } from '@angular/forms'; // NgModel lives here
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent
   ],
   imports: [
+    //basic angular imports that will occur with 
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // tutorial guided imports
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

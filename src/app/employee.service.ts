22a3c4fd3f69f86229291c4ac6d60a4386ea71a0
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-08 16:36:51 -0400

    more ng generated scaffolding. removal of unused css features

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -6,17 +6,19 @@ import { Observable, of } from 'rxjs';
 //Link to mock data and object defintiion
 import { Employee } from  './employee';
 import { EMPLOYEES } from './mock-employees';
+import { MessageService } from './message.service';
 
 
 //Provided from ng generate, which will now allow angular to inject this script as needed
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   
   getEmployees(): Observable<Employee[]> {
     const employees = of(EMPLOYEES);
+    this.messageService.add('EmployeeService: fetch employees');
     return employees;
   }
 

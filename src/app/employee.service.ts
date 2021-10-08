15e4a48fc9bbe1c27b51c01f3014f80ff3d14f6c
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-08 15:46:22 -0400

    adding service to deal with asynchronous future data movement

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+//Link to mock data and object defintiion
+import { Employee } from  './employee';
+import { EMPLOYEES } from './mock-employees';
+// key class of RxJS Library https://rxjs.dev/
+import { Observable, of } from 'rxjs';
 
+//Provided from ng generate, which will now allow angular to inject this script as needed
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  getEmployees(): Observable<Employee[]> {
+    const employee = of(EMPLOYEES);
+    return employee;
+  }
 
   constructor() { }
 }
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
-import { EMPLOYEES } from '../mock-employees';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employees',
@@ -8,17 +7,22 @@ import { EMPLOYEES } from '../mock-employees';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
-  employees = EMPLOYEES;
-  selectedEmployee?: Employee;
+  employees : Employee[] = [];
 
-  constructor() {
+  constructor(private employeeService: EmployeeService) {
 
    }
 
-  ngOnInit(): void {
+   // Currently has a synchronous signature. Would not be practical in real app, as server might have to return this data to angular components
+  getEmployees(): void {
+    this.employeeService.getEmployees()
+      .subscribe(employees => this.employees = employees);
   }
 
-  
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
   onSelect(employee: Employee): void {
     this.selectedEmployee = employee;
   }

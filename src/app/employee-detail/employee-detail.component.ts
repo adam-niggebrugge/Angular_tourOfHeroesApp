b8c201b66514ce4cf6487645e96aa7d4c8e1425c
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-26 18:26:37 -0400

    updated details to accept routes and go back using angular navigational hooks

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Employee } from '../employee';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { EmployeeService } from '../employee.service'
+
 @Component({
   selector: 'app-employee-detail',
   templateUrl: './employee-detail.component.html',
@@ -9,9 +14,23 @@ import { Employee } from '../employee';
 export class EmployeeDetailComponent implements OnInit {
   @Input() employee?: Employee;
 
-  constructor() { }
+  constructor( 
+    private route: ActivatedRoute, //holds information about route instance to EmployeeDetailComponent
+    private employeeService: EmployeeService, //used to get data from service, could be server db (tutorial mock data source)
+    private location: Location, //angular service for browser ineraction, like navigating back
+  ) { }
 
   ngOnInit(): void {
+    this.getEmployee(); 
+  }
+  //Now that routes are being used to open a details view - logic is required to move objects between components
+  getEmployee(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.employeeService.getEmployee(id)
+    .subscribe(employee => this.employee = employee)
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }

ae8d6fecb37a5d4dcd41c7dbef06eb6ba0802f34
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-26 12:31:45 -0400

    initial angular scaffolding of navigation routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesComponent } from './employees/employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //typical Angular route made of two components
+  { path: 'employees', //a string to match the URL
+    component: EmployeesComponent //what to display when a URL like localhost:4200/employees is given
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], //method forRoot() since configured at app root level will make this deployable for any service provider and base URL given
+  exports: [RouterModule] //enables the RouterModule to be available throughout the app
 })
 export class AppRoutingModule { }

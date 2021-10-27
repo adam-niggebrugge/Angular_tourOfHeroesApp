6f839517ca778bd6042dc1cf1b1d646e2cc1eb1a
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-27 13:41:41 -0400

    create mock data, function review required of id since not following pattern

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,9 +1,73 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Employee } from './employee';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const employees = [
+      { 
+        id: 101,
+        name: 'Adam Niggebrugge',
+        role: "Production Engineer",
+    },
+    { 
+        id: 1051,
+        name: 'Tom Weeks',
+        role: "Production Engineer",
+    },
+    { 
+        id: 21,
+        name: 'Matt Nordmeyer',
+        role: "Prodution Electrical Engineer Manager",
+    },
+    { 
+        id: 222,
+        name: 'Ethan Stallman',
+        role: "Production Engineer",
+    },
+    { 
+        id: 3811,
+        name: 'Marwan Dayeh',
+        role: "Production Engineer",
+    },
+    { 
+        id: 3939,
+        name: 'Kirstie Schnieder',
+        role: "Production Engineer",
+    },
+    { 
+        id: 659,
+        name: 'Brady Dufau',
+        role: "Quotation Engineer Manager",
+    },
+    {
+        id: 12,
+        name: 'Nicholas Lang',
+        role: "R&D Engineer",
+    },
+    {
+        id: 131,
+        name: 'Anthony Soellner',
+        role: "R&D Engineer",
+    },
+    {
+        id: 28,
+        name: 'Benjamin Flick',
+        role: "Engineering Manager",
+    },
+    ];
+    return {employees};
+  }
 
-  constructor() { }
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(employees: Employee[]): number {
+    return employees.length > 0 ? Math.max(...employees.map(e => e.id)) + 1 : 11;
+  }
 }

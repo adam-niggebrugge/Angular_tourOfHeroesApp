e20c0f00b6e047ffcd695b397b093fbc84044dc3
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-27 14:11:28 -0400

    employee service now set for data movement over http

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-
+//for connection to data source
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 // key class of RxJS Library https://rxjs.dev/
 import { Observable, of } from 'rxjs';
+//To catch errors, you "pipe" the observable result from http.get() through an RxJS catchError() operator.
+import { catchError, map, tap } from 'rxjs/operators';
 
 //Link to mock data and object defintiion
 import { Employee } from  './employee';
@@ -14,19 +17,72 @@ import { MessageService } from './message.service';
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor(private messageService: MessageService) { }
+
+  /**
+   *  form :base/:collectionName with the address of the employees resource on the server. Here 
+   * base is the resource to which requests are made, and collectionName is the employees data 
+   * object in the in-memory-data-service.ts
+   * */
+  private employeesUrl = 'api/employees';  // URL to web api
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient,
+  ) { }
   
-  getEmployees(): Observable<Employee[]> {
-    const employees = of(EMPLOYEES);
-    this.messageService.add('EmployeeService: fetched employees');
-    return employees;
+  /** Log a EmployeeService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`EmployeeService: ${message}`);
   }
 
-  getEmployee(id: number): Observable<Employee> {
-  // For now, assume that an employee with the specified `id` always exists.
-  // Error handling will be added in the next step of the tutorial.
-  const employee = EMPLOYEES.find(e => e.id === id)!;
-  this.messageService.add(`EmployeeService: fetched employee id=${id}`);
-  return of(employee);
+  getEmployees(): Observable<Employee[]> {
+    //when using RxJS mock
+    //   const employees = of(EMPLOYEES);
+    //  // this.messageService.add('EmployeeService: fetched employees'); //with private log moving where messages would reside
+    //   return employees;
+    return this.http.get<Employee[]>(this.employeesUrl)
+      .pipe(
+        // RxJS tap() operator, which looks at the observable values, does something with those values, and passes them along. The tap() call back doesn't touch the values themselves.
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Employee[]>('getEmployees', []))
+      );
   }
+
+  //
+  // getEmployee(id: number): Observable<Employee> {
+  //   // For now, assume that an employee with the specified `id` always exists.
+  //   // Error handling will be added in the next step of the tutorial.
+  //   const employee = EMPLOYEES.find(e => e.id === id)!;
+  //   this.messageService.add(`EmployeeService: fetched employee id=${id}`);
+  //   return of(employee);
+  // }
+
+  /** GET employee by id. Will 404 if id not found */
+  getEmployee(id: number): Observable<Employee> {
+    const url = `${this.employeesUrl}/${id}`;
+    return this.http.get<Employee>(url).pipe(
+      tap(_ => this.log(`fetched employee id=${id}`)),
+      catchError(this.handleError<Employee>(`getEmployee id=${id}`))
+    );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 }

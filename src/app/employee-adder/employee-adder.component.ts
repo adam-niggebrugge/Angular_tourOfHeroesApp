411d8b826c88205a64d89284f7577653965d4791
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-28 09:27:00 -0400

    tutorial deviation add new object through component

--- src/app/employee-adder/employee-adder.component.ts
+++ src/app/employee-adder/employee-adder.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service'
+
 
 @Component({
   selector: 'app-employee-adder',
@@ -6,10 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-adder.component.css']
 })
 export class EmployeeAdderComponent implements OnInit {
+  @Input() employee?: Employee;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, //holds information about route instance to EmployeeDetailComponent
+    private employeeService: EmployeeService,
+    private location: Location, //angular service for browser ineraction, like navigating back
+  ) { }
 
   ngOnInit(): void {
   }
 
+  add(name: string, position: string): void {
+    name = name.trim();
+    if(!name && !position) { return; }
+    // if neglect subscribe(), the service will not send the request to the server. As a rule, an Observable does nothing until something subscribes.
+    this.employeeService.addEmployee( { name, position } as Employee ).subscribe(() => this.goBack());
+      // .subscribe(employee => {
+      //   this.employees.push(employee);
+      // })
+  }
+
+
+  goBack(): void {
+    this.location.back();
+  }
 }
